80cd150b42b63b311c214f40767d1485085e7221
Odei <odei.cebanc@gmail.com>
2025-11-14 12:22:00 +0100

    Creacion de login register y token

--- contactos/index.js
+++ contactos/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
 import cors from 'cors';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const port = 3000;
@@ -14,6 +16,9 @@ app.use(cors());
 const uri = 'mongodb+srv://glu_db_user:8aa8ii1oo1@cluster0.te6deme.mongodb.net/';
 const dbName = 'ProviSys';
 
+// Clave secreta para JWT
+const SECRET_KEY = "MIDDLE-DEEP-HOME-TOOK-FLOOR";
+const EXPIRES_IN = "24h";
 
 // USUARIOS---> APARTADO DE LA API PARA CARGAR DATOS DE USUARIOS
 let usuarios; // colección compartida por las rutas
@@ -38,34 +43,110 @@ async function init() {
   // 📄 GET /usuarios → listar todos
   app.get('/usuarios', async (req, res) => {
     const docs = await usuarios.find().toArray();
-    console.log(docs);
+    //console.log(docs);
     res.json(docs);
   });
 
   // GET /productos -> obtener uno por id_product
   app.get('/productos', async (req, res) => {
     const docs = await productos.find().toArray();
-    console.log(docs);
+    //console.log(docs);
     res.json(docs);
   });
 
   app.get('/posts',async (req,res)=>{
     const docs=await posts.find().toArray();
-    console.log(docs);
+    //console.log(docs);
     res.json(docs);
   });
 
-  // 🔎 GET /usuarios/:id_user → obtener uno por id_user
-  app.get('/usuarios/:id_user', async (req, res) => {
-    const { id_user } = req.params;
-    const id = parseInt(id_user);
-    const doc = await usuarios.findOne({ id_user: id });
+    app.post('/registrar', async (req, res) => {
+    const { name, email, profile_picture, password } = req.body;
 
-    console.log(doc);
-    if (!doc) return res.status(404).json({ error: 'No encontrado' });
-    res.json(doc);
+    if (!name || !email || !profile_picture || !password)
+      return res.status(400).json({ error: 'nombre, email, foto de perfil y contraseña son obligatorios' });
+
+    // Hash de la contraseña
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Generar un id_user único
+    const ultimoUsuario = await usuarios.find().sort({ id_user: -1 }).limit(1).toArray();
+    const nuevoIdUser = ultimoUsuario.length > 0 ? ultimoUsuario[0].id_user + 1 : 1;
+
+    const nuevo = { id_user: nuevoIdUser, name, email, profile_picture, passwordHash };
+
+    const r = await usuarios.insertOne(nuevo);
+    res.status(201).json({ id_user: nuevoIdUser, ...nuevo });
   });
+  
+  app.post('/Login', async (req, res) => {
+      const { name, password } = req.body;
 
+      // Buscar usuario por name
+      const db_user = await usuarios.findOne({ name });
+
+      if (!db_user) {
+        return res.status(400).json({ detail: "Nombre de usuario incorrecto" });
+      }
+
+      // Comprobar contraseña
+      const passwordMatch = await bcrypt.compare(password, db_user.passwordHash);
+
+
+      if (!passwordMatch) {
+        return res.status(400).json({ detail: "Contraseña incorrecta" });
+      }
+
+      // Crear token
+      const token = jwt.sign(
+        { sub: String(db_user.id_user) },
+        SECRET_KEY,
+        { expiresIn: EXPIRES_IN }
+      );
+
+      return res.json({
+        message: "Login exitoso",
+        usuario: db_user.nombre_usuario,
+        access_token: token,
+        token_type: "bearer",
+      });
+    });
+
+    app.get('/usuarios/me', async (req, res) => {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) return res.status(401).json({ error: 'No autorizado' });
+
+      // Obtener el token del header "Bearer <token>"
+      const token = authHeader.split(' ')[1];
+      if (!token) return res.status(401).json({ error: 'Token inválido' });
+
+      try {
+        // Verificar token JWT
+        const payload = jwt.verify(token, SECRET_KEY);
+
+        // Buscar usuario por id_user almacenado en el token
+        const usuario = await usuarios.findOne({ id_user: parseInt(payload.sub) });
+
+        if (!usuario) return res.status(404).json({ error: 'Usuario no encontrado' });
+
+        // Devolver solo los campos que quieras exponer
+        res.json({id_user: usuario.id_user,});
+      } catch (err) {
+        console.error(err);
+        res.status(401).json({ error: 'Token inválido o expirado' });
+      }
+    });
+
+    // 🔎 GET /usuarios/:id_user → obtener uno por id_user
+    app.get('/usuarios/:id_user', async (req, res) => {
+      const { id_user } = req.params;
+      const id = parseInt(id_user);
+      const doc = await usuarios.findOne({ id_user: id });
+
+      //console.log(doc);
+      if (!doc) return res.status(404).json({ error: 'No encontrado' });
+      res.json(doc);
+    });
 //PROVEEDORES--> APARTADO DE LA API PARA CARGAR DATOS DE PROVEEDORES
 
   app.get('/proveedores', async (req, res) => {
@@ -91,19 +172,9 @@ async function init() {
     const { id_product } = req.params;
     const id = parseInt(id_product);
     const doc = await productos.findOne({ id_product: id });
-    console.log(doc);
+    //console.log(doc);
     if (!doc) return res.status(404).json({ error: 'No encontrado' });
     res.json(doc);
-  });
-
-  // ➕ POST /usuarios → crear
-  app.post('/usuarios', async (req, res) => {
-    const { name, email, profile_picture } = req.body;
-    if (!name || !email || !profile_picture) return res.status(400).json({ error: 'nombre, email y foto de perfil son obligatorios' });
-
-    const nuevo = { name, email, profile_picture };
-    const r = await usuarios.insertOne(nuevo);
-    res.status(201).json({ id_user: r.insertedId, ...nuevo });
   });
 
   // POST /Productos → crear
