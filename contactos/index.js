1927a8d08ee9dc32e9a5a992fbec1c353d6112f2
Gari <garikworkaddress@gmail.com>
2025-11-17 08:50:36 +0100

    arreglo que la había liado parda (oops)

--- contactos/index.js
+++ contactos/index.js
@@ -159,7 +159,15 @@ async function init() {
     }
   });
 
-  app.get('/proveedores/:id_user', async (req, res) => {
+  app.get('/proveedores/:id_provider', async (req, res) => {
+    const { id_provider } = req.params;
+    const id = parseInt(id_provider);
+    const doc = await proveedores.findOne({ id_provider: id });
+    if (!doc) return res.status(404).json({ error: 'No encontrado' });
+    res.json(doc);
+  });
+
+  app.get('/proveedores/porUser/:id_user', async (req, res) => {
     const { id_provider } = req.params;
     const id = parseInt(id_provider);
     const doc = await proveedores.findOne({ id_user: id });
